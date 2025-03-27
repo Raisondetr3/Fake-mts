76b49652e11dd130896e8a79f7992eb628448a3e
kamil <camgal04@gmail.com>
2025-03-27 19:17:08 +0300

    configure for wildfly

--- src/main/java/ru/itmo/fake_mts/FakeMtsApplication.java
+++ src/main/java/ru/itmo/fake_mts/FakeMtsApplication.java
@@ -3,14 +3,21 @@ package ru.itmo.fake_mts;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class FakeMtsApplication {
+public class FakeMtsApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FakeMtsApplication.class);
+    }
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-		SpringApplication.run(FakeMtsApplication.class, args);
-	}
+        SpringApplication.run(FakeMtsApplication.class, args);
+    }
 }
