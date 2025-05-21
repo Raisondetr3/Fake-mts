df703fe26fd3da7455db255d1e1b5776c9bca34d
Raisondetre <aakuma1337@gmail.com>
2025-05-21 19:38:14 +0300

    Fix loading application.properties in jar

--- node_a_core/src/main/java/ru/itmo/node_a_core/config/MqttProps.java
+++ node_a_core/src/main/java/ru/itmo/node_a_core/config/MqttProps.java
@@ -2,6 +2,7 @@ package ru.itmo.node_a_core.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
